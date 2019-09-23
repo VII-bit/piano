87f0838383f4071c305d62de58fef9140246ef2b
VII <vladislavivanov95@gmail.com>
2019-09-23 17:16:52 +0300

    2

--- script.js
+++ script.js
@@ -3,9 +3,46 @@ function changeColor() {
 	let key = event.code;
 	if (key == `${key}`) {
 		document.getElementById(key).style.backgroundColor = 'grey';
-		if (key == 'Digit1') {
-		let audio = new Audio('');
+		
+		switch (key) {
+
+		case 'Digit1':
+		let audio = new Audio('sounds/A3vH.wav');
 		audio.play();
+		break;
+
+		case 'Digit3':
+		let audio1 = new Audio('sounds/A3vH.wav');
+		audio1.play();
+		break;
+
+		case 'Digit5':
+		let audio2 = new Audio('sounds/A3vH.wav');
+		audio2.play();
+		break;
+
+		case 'Digit6':
+		let audio3 = new Audio('sounds/A3vH.wav');
+		audio3.play();
+		break;
+
+		case 'Digit8':
+		let audio4 = new Audio('sounds/A3vH.wav');
+		audio4.play();
+		break;
+
+		case 'Digit0':
+		let audio5 = new Audio('sounds/A3vH.wav');
+		audio5.play();
+		break;
+
+		case 'Equal':
+		let audio6 = new Audio('sounds/A3vH.wav');
+		audio6.play();
+		break;
+
+		default:
+    	break;
 		}
 	}
 }
